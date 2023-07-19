3ebfc095bc99a7676b80bfea6aea20ef6416033b
redwing45 <19aar94@gmail.com>
2023-07-19 01:40:04 -0500

    propsnotimporting

---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+export default function Banner() {
+  return (
+    <div>
+        <header>"Orbit Report"</header>
+        <p>Click on buttons to see satellite orbits!</p>
+    </div>
+  )
+}
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,23 @@
+import satData from "./satData";
+import App from "../App";
+
+
 const Buttons = () => {
+  <>
+   { displaySats.map((id,sat) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <button key ={id} onClick={filterByType()}>{sat} Orbit  </button>
       </div>
   );
-};
+    })}
 
+    <button onClick={setSat(satData)}></button>
+      <button onClick={displaySats}>All Orbits</button>
+      <button>All Orbits</button>
+  
+</>
+}
 export default Buttons;
+
+
